6b9bf8f543b63e1fe727a259475e36f2f943c929
iop125 <232940202@qq.com>
2019-05-19 14:08:24 +0800

    s

--- src/main/java/com/my/common/util/RedisUtil.java
+++ src/main/java/com/my/common/util/RedisUtil.java
@@ -3,6 +3,10 @@ package com.my.common.util;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class RedisUtil {
 
     public void redisPool(){
@@ -26,6 +30,25 @@ public class RedisUtil {
     public static void main(String [] a){
         RedisUtil redisUtil = new RedisUtil();
         redisUtil.redisPool();
+        System.out.println( redisUtil.lambdaMaxInteger());
+        String a1 = "a";
+//        Optional
+////                .ofNullable(a)
+////                .map(k->k+"bc")
+////                .filter(::hasMultibyte)
+////                .orElseThrow(()->new Exception("错误"));
 
+    }
+
+    public int lambdaMaxInteger() {
+        List<Integer> integers = new ArrayList<Integer>();
+        integers.add(1);
+        integers.add(129);
+        integers.add(1111);
+        integers.add(93);
+        integers.add(66);
+        integers.add(7);
+        integers.add(2);
+        return integers.stream().reduce(Integer.MIN_VALUE, (a, b) -> Integer.max(a,b));
     }
 }
